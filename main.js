2e7958f34350eb21946098efe0e2d2f59bdfffb9
Mudassir ijaz <2502106@students.au.edu.pk>
2026-02-21 17:00:03 +0500

    JavaScript Practise.
    JavaScript practise by using arrows function, arrays, filter, product, module.

---
+++ main.js
@@ -0,0 +1,9 @@
+// main.js
+import { add, subtract } from './math-utils.js';
+
+// Using module functions
+const sum = add(10, 5);
+const difference = subtract(20, 7);
+
+console.log('Sum:', sum);           // Sum: 15
+console.log('Difference:', difference); // Difference: 13
---
+++ math-utils.js
@@ -0,0 +1,8 @@
+// math-utils.js
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
